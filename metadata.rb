aa99d49056cb0f694817e8c4ace9f115edb56fd2
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-07-13 10:13:25 +0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "locale"
+maintainer "Nestor G Pestelos Jr"
+maintainer_email "ngpestelos@gmail.com"
+description "Fixes the LC_ALL not found problem"
+version "0.1.0"
+recipe "locale:default", ""
+
+%w { ubuntu debian }.each do |os|
+  supports os
+end
---
+++ recipes/default.rb
@@ -0,0 +1,3 @@
+cookbook_file "/etc/default/locale" do
+  source "locale"
+end
